442da169627d07ea72950f88901d7128a5f3a2ab
BCrlrd <4371755@gmail.com>
2014-12-01 17:06:40 +0100

    Fixed the position
    Fixed

--- src/Budget.java
+++ src/Budget.java
@@ -8,7 +8,7 @@ public class Budget {
 	private static int budg_vir;
 
 	public static void main(String[] args) {
-		Player p = new Player("Frits","Fritsman","MV",19,100000,100,100,100,100,100,100);
+		Player p = new Player("Frits","Fritsman","RB",19,100000,100,100,100,100,100,100);
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Wat is uw budget? ");
 		budg_rel=budg_vir=sc.nextInt();
