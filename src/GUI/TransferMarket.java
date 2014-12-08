cd52dd356b85250d6c69f224a63d757a01c1faa8
thervh70 <R.H.vanHeukelum@student.tudelft.nl>
2014-12-08 17:04:03 +0100

    Added background and changed layout

--- src/GUI/TransferMarket.java
+++ src/GUI/TransferMarket.java
@@ -7,6 +7,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Font;
@@ -21,17 +23,17 @@ public class TransferMarket {
 		Text forSale = new Text("For Sale");
 		Text myProducts = new Text("My Products :)");
 		Font font = new Font("Arial", 35);
-		Line line = new Line();
 		
-		line.setEndX(1770);
-		line.setLayoutY(820);
+		Image background = new Image("/GUI/Resources/background_transfer-market.png");
+		ImageView imgView = new ImageView(background);
+		root.getChildren().add(imgView);
 		
 		forSale.setFont(font);
-		forSale.setLayoutX(105);
-		forSale.setLayoutY(90);
+		forSale.setLayoutX(125);
+		forSale.setLayoutY(220);
 		myProducts.setFont(font);
-		myProducts.setLayoutX(1005);
-		myProducts.setLayoutY(90);
+		myProducts.setLayoutX(1000);
+		myProducts.setLayoutY(220);
 		
 		back.setFont(new Font("Arial", 25));
 		back.setLayoutX(80);
@@ -40,14 +42,14 @@ public class TransferMarket {
 		TableView<String> tableForSale = new TableView();
 		tableForSale.setEditable(false);
 		tableForSale.setPrefSize(801, 540);
-		tableForSale.setLayoutX(100);
-		tableForSale.setLayoutY(100);
+		tableForSale.setLayoutX(125);
+		tableForSale.setLayoutY(250);
 		tableForSale.setEditable(false);
 		
 		TableView<String> tableMyPlayers = new TableView();
 		tableMyPlayers.setPrefSize(720, 540);
 		tableMyPlayers.setLayoutX(1000);
-		tableMyPlayers.setLayoutY(100);
+		tableMyPlayers.setLayoutY(250);
 		tableMyPlayers.setEditable(false);
 		
 		TableColumn name = new TableColumn("Name");
@@ -76,7 +78,7 @@ public class TransferMarket {
 		tableMyPlayers.getColumns().addAll(name,position,age,worth,shooting,passing,dribbling,defending,physical);
 
 		
-		root.getChildren().addAll(back, tableForSale, forSale, tableMyPlayers, myProducts, line);
+		root.getChildren().addAll(back, tableForSale, forSale, tableMyPlayers, myProducts);
 		
 		back.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
