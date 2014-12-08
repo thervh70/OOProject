4182af1672d451a9d3a52adf104a5ad834a76b3f
BCrlrd <4371755@gmail.com>
2014-12-08 15:47:03 +0100

    Updated budget
    Mhm.

--- src/Budget.java
+++ src/Budget.java
@@ -1,32 +1,44 @@
 /**Beginning of our budget class 
  * @author Bram
  */
-import Database.Player;
-import java.util.*;
+
+import Database.*;
 public class Budget {
 	private static int budg_rel;
 	private static int budg_vir;
 
 	public static void main(String[] args) {
-		Player p = new Player("Frits","Fritsman","RB",19,100000,100,100,100,100,100,100);
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Wat is uw budget? ");
-		budg_rel=budg_vir=sc.nextInt();
-		sc.close();
-		System.out.println(bieden(p));
-		System.out.println(budg_rel);
-		System.out.println(budg_vir);
+		Player p = new Fieldplayer("Frits","Fritsman","RB",19,100000,100,100,100,100,100,100);
+		budg_rel=100000000;
+		budg_vir=100000000;
+		for(int i=0;i<10000;i++){
+			System.out.println(bieden(p));
+			/**System.out.println(budg_rel);
+			System.out.println(budg_vir);**/
+		}
 	}
 	
 	public static boolean bieden(Player p){
-		budg_vir-=p.getPri();
-		if (p.getPri()<budg_rel){
-			budg_rel-=p.getPri();
-			return true;
+		if(budg_vir<p.getPri()){
+			return false;
 		}
 		else{
-			budg_vir+=p.getPri();
-			return false;
+			budg_vir-=p.getPri();
+			if (p.getPri()<budg_rel){
+				if(Math.random()>0.6){
+					budg_rel-=p.getPri();
+					System.out.println(budg_rel);
+					return true;
+				}
+				else{
+					budg_vir+=p.getPri();
+					return false;
+				}
+			}
+			else{
+				budg_vir+=p.getPri();
+				return false;
+			}
 		}
 	}
 }
