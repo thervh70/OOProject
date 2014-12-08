fe8632d82b7854b670d4d745ff79e7644d8ce54b
thervh70 <R.H.vanHeukelum@student.tudelft.nl>
2014-12-08 17:03:51 +0100

    Added background and changed layout

--- src/GUI/TeamManagement.java
+++ src/GUI/TeamManagement.java
@@ -7,6 +7,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Font;
@@ -18,10 +20,10 @@ public class TeamManagement {
 	public static void start(Stage primaryStage) {
 		Pane root = new Pane();
 		Button back = new Button("Back to Management Center");
-		Line line = new Line();
 		
-		line.setEndX(1770);
-		line.setLayoutY(820);
+		Image background = new Image("/GUI/Resources/background_team-management.png");
+		ImageView imgView = new ImageView(background);
+		root.getChildren().add(imgView);
 		
 		Text t1 = new Text(400,50,"Set-up");
 		Text t2 = new Text(400,500, "Bench");
@@ -30,16 +32,16 @@ public class TeamManagement {
 		TableView<String> tableSetup = new TableView();
 		tableSetup.setEditable(false);
 		tableSetup.setPrefSize(700, 540);
-		tableSetup.setLayoutX(100);
-		tableSetup.setLayoutY(100);
+		tableSetup.setLayoutX(125);
+		tableSetup.setLayoutY(200);
 		tableSetup.setEditable(false);
-		
+
 		//Create a table for the bench with fixed columns
 		TableView<String> tableBench = new TableView();
 		tableBench.setEditable(false);
 		tableBench.setPrefSize(700, 540);
-		tableBench.setLayoutX(950);
-		tableBench.setLayoutY(100);
+		tableBench.setLayoutX(1000);
+		tableBench.setLayoutY(200);
 		tableBench.setEditable(false);
 		
 		//Creat columns for both tables
@@ -87,7 +89,7 @@ public class TeamManagement {
 		back.setLayoutY(870);
 		
 		//Add elements to the canvas
-		root.getChildren().addAll(back, tableSetup, tableBench, line);
+		root.getChildren().addAll(back, tableSetup, tableBench);
 		
 		back.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
