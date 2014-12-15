173083cc16eb50ac943388b1761595b257f94e3b
Bram <4371755@gmail.com>
2014-12-15 14:12:13 +0100

    everything is now broken
    mathiasplsfix

--- src/Budget.java
+++ src/Budget.java
@@ -4,40 +4,26 @@
 
 import Database.*;
 public class Budget {
-	private static int budg_rel;
-	private static int budg_vir;
+	DBmain test = XmlParser.parseDB();
 
 	public static void main(String[] args) {
 		Player p = new Fieldplayer("Frits","Fritsman","RB",19,100000,100,100,100,100,100,100);
-		budg_rel=100000000;
-		budg_vir=100000000;
-		for(int i=0;i<100;i++){
-			System.out.println(bieden(p));
-			/**System.out.println(budg_rel);
-			System.out.println(budg_vir);**/
-		}
 	}
 	
-	public static boolean bieden(Player p){
-		if(budg_vir<p.getPri()){
-			return false;
-		}
-		else{
+	public static void bieden(Player p){
+		if(!(budg_vir<p.getPri())){
 			budg_vir-=p.getPri();
 			if (p.getPri()<budg_rel){
 				if(Math.random()>0.6){
 					budg_rel-=p.getPri();
 					System.out.println(budg_rel);
-					return true;
 				}
 				else{
 					budg_vir+=p.getPri();
-					return false;
 				}
 			}
 			else{
 				budg_vir+=p.getPri();
-				return false;
 			}
 		}
 	}
