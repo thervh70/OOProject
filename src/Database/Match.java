96a75e55586aa351467c9110954519242a03dac1
marcelkuipers1996 <marcel96@outlook.com>
2014-12-19 22:45:50 +0100

    scheduler and match basic

---
+++ src/Database/Match.java
@@ -0,0 +1,24 @@
+package Database;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Match {
+
+	SimpleStringProperty tHome;
+	SimpleStringProperty tAway;
+	SimpleIntegerProperty gHome;
+	SimpleIntegerProperty gAway;
+	
+	private Team teamHome, teamAway;
+	private int goalsHome, goalsAway, day;
+	boolean played;
+	
+	public Match(Team teamHome, Team teamAway, int day){
+		this.teamHome = teamHome;
+		this.teamAway = teamAway;
+		this.played = false;
+		this.day = day;
+	}
+	
+}
