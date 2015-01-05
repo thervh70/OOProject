f47c713abcb7e7297997fd59d5a7dabae08ef3ba
marcelkuipers1996 <marcel96@outlook.com>
2015-01-05 14:44:56 +0100

    schedule almost finished

--- src/Model/Competition.java
+++ src/Model/Competition.java
@@ -1,5 +1,26 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Competition {
 
+	private ArrayList<Match> matches = new ArrayList<Match>();
+	
+	
+	public void add(Match match){
+		matches.add(match);
+	}
+	
+	public String toString(){
+		return matches.toString();
+	}
+	
+	public int getSize(){
+		return matches.size();
+	}
+	
+	public Match get(int index){
+		return matches.get(index);
+	}
+	
 }
--- src/Model/Scheduler.java
+++ src/Model/Scheduler.java
@@ -1,49 +1,62 @@
 package Model;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class Scheduler {
 
 	public static void main(String[] args){
-		//Team[] teamOrder = new Team[18];
-		ArrayList<Team> teams = new ArrayList<Team>();
-		List<Team> teamOrder = new ArrayList<Team>();
-		DBmain dbmain = XmlParser.parseDB();
-		Team teamHome;
-		Team teamAway;
+		List<Team> TeamOrder = new ArrayList<Team>();
+		int number;
+		DBmain d = XmlParser.parseDB();
+		Competition comp = new Competition();
 		for (int i = 0; i < 17; i++) {
-			teams.add(dbmain.getTeam(i));
-		}
-		for (int i = 0; i < 17; i++) {
-			int number = (int) Math.round((Math.random()* 18- 0.5));
-			while(teamOrder.contains(dbmain.getTeam(number))){
+			number = (int) Math.round((Math.random()* 18- 0.5));
+			while(TeamOrder.contains(d.getTeam(number))){
 				number = (int) Math.round((Math.random()* 18- 0.5));
 			}
-			teamOrder.add(dbmain.getTeam(number));
+			TeamOrder.add(d.getTeam(number));
 		}
-		for (int day = 0; day < 33; day++) {
-		
-			if(day<=18){
-				for (int i = 0; i < 8; i++) {
-					int numberTeamHome = day + 2*i + 1;
-					if(numberTeamHome > 17 ){
-						numberTeamHome -=18;
-					}
-					int numberTeamAway = numberTeamHome - day;
-					if(numberTeamAway < 0){
-						numberTeamAway += 18;
-					}
-					teamHome = teamOrder.get(numberTeamHome);
-					teamAway= teamOrder.get(numberTeamAway);
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder;
+		try {
+			builder = factory.newDocumentBuilder();
+			Document document = builder.parse("src/Model/Resources/Schedule.xml");
+			NodeList division = document.getDocumentElement().getChildNodes();
+			for(int i=1;i<division.getLength();i+=2) {
+		    	Node team = division.item(i);
+		    	NodeList matchattrs = team.getChildNodes();
+		    	Match match = new Match(Integer.parseInt(matchattrs.item(1).getTextContent()),Integer.parseInt(matchattrs.item(3).getTextContent()),Integer.parseInt(matchattrs.item(5).getTextContent()));
+		    	comp.add(match);
+		    	
+		    }
+		} 
+		catch (ParserConfigurationException | IOException | SAXException e) {
+			e.printStackTrace();
+		} 
+		  
+		for (int i = 0; i < comp.getSize(); i++) {
+			for (int j = 0; j < 17; j++) {
+				Match match =comp.get(i);
+				if(match.getHomeIndex() == j){
+					match.setTeamHome(TeamOrder.get(j));
 				}
-				
-				
-			}
-			else{
-				
-			}
-			
+				if(match.getAwayIndex() == j){
+					match.setTeamAway(TeamOrder.get(j));
+				}
+				System.out.println(i);			}
 		}
+		System.out.println(comp.toString());
+		
 	}
 }
