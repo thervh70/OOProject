a1f9b9c795cf8cd9d5880895cb4ddeb23ea9997a
thervh70 <R.H.vanHeukelum@student.tudelft.nl>
2015-01-10 17:38:31 +0100

    Renewed Management Center GUI
    To show some additional information on main screen

--- src/View/ManagementCenter.java
+++ src/View/ManagementCenter.java
@@ -1,12 +1,16 @@
 package View;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.SAXException;
 
 import Controller.saveGame;
+import Model.Competition;
+import Model.Match;
+import Model.Team;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
@@ -30,11 +34,11 @@ public class ManagementCenter {
 		Button save = new Button("Save Game");
 		Button menu = new Button("Back to Main Menu");
 		
-		Style.setButtonStyle(manage, 50);
-		Style.setButtonStyle(table, 50);
-		Style.setButtonStyle(match, 50);
-		Style.setButtonStyle(market, 50);
-		Style.setButtonStyle(save, 50);
+		Style.setButtonStyle(manage, 55);
+		Style.setButtonStyle(table, 55);
+		Style.setButtonStyle(match, 55);
+		Style.setButtonStyle(market, 55);
+		Style.setButtonStyle(save, 55);
 		
 		Style.setButtonStyle(menu, 45);
 
@@ -42,10 +46,43 @@ public class ManagementCenter {
 
 		Style.setLocation(menu, 150, 870);
 		
-		VBox vbox = new VBox(15);
+		VBox vbox = new VBox(10);
 		vbox.getChildren().addAll(manage, table, match, market, save);
-		Style.setLocation(vbox, 775, 220);
+		Style.setLocation(vbox, 350, 220);
 		vbox.setAlignment(Pos.CENTER);
+		
+		Text day = new Text("Day: " + saveGame.getDay());
+		Style.setTextStyle(day, 90);
+		
+		Competition comp = saveGame.getCompetition();
+		String opp = null;
+		
+		ArrayList<Match> gamesToday = comp.getMatchesForDay(saveGame.getDay());
+		for(Match game : gamesToday){
+			Team home = game.getTeamHome();
+			Team away = game.getTeamAway();
+			
+			if(home.getNm().equals(saveGame.getMyTeamName())){
+				opp = away.getNm();
+			}
+			
+			else if(away.getNm().equals(saveGame.getMyTeamName())){
+				opp = home.getNm();
+			}
+		}
+		
+		Text opponent = new Text("Next opponent: " + opp);
+		Style.setTextStyle(opponent, 60);
+		
+		Text space = new Text("");
+		
+		Text budget = new Text("Budget: " + saveGame.getMyTeam().getBdgt_vir());
+		Style.setTextStyle(budget, 60);
+		
+		VBox vbox2 = new VBox(10);
+		vbox2.getChildren().addAll(day, opponent,space,budget);
+		Style.setLocation(vbox2, 1100, 240);
+		vbox2.setAlignment(Pos.CENTER);
 		
 		manage.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -147,7 +184,7 @@ public class ManagementCenter {
 			}
 		});
 		
-		root.getChildren().addAll(vbox, menu);
+		root.getChildren().addAll(vbox, vbox2, menu);
 		
 		primaryStage.getScene().setRoot(root);
 		primaryStage.show();
