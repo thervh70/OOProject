252545ca8c4a86c002430bb4664d6559412293d0
marcelkuipers1996 <marcel96@outlook.com>
2015-01-17 19:51:01 +0100

    test update

--- src/Tests/CompetitionTest.java
+++ src/Tests/CompetitionTest.java
@@ -15,25 +15,61 @@ import Model.XmlParser;
 public class CompetitionTest {
 
 	@Test
+	public void testAdd(){
+		DBmain db = XmlParser.parseDB();
+		Team team = db.getTeam(0);
+		Team team2 = db.getTeam(1);
+		Match match = new Match(1,team, team2);
+		Competition comp = new Competition();
+		Competition comp2 = new Competition();
+		Competition comp3 = new Competition();
+		comp.add(match);
+		comp2.add(match);
+		comp2.add(match);
+		assertFalse(comp.equals(comp3));
+		assertTrue(comp.equals(comp2));
+	}
+	
+	@Test
+	public void testRemove(){
+		DBmain db = XmlParser.parseDB();
+		Team team = db.getTeam(0);
+		Team team2 = db.getTeam(1);
+		Match match = new Match(1,team, team2);
+		Match match2 = new Match(2,team, team2);
+		Competition comp = new Competition();
+		Competition comp2 = new Competition();
+		Competition comp3 = new Competition();
+		comp.add(match);
+		comp2.add(match);
+		comp2.add(match2);
+		comp2.remove(match2);
+		assertTrue(comp.equals(comp2));
+	}
+	
+	@Test
 	public void testEquals(){
 
 		DBmain db = XmlParser.parseDB();
 		Team team = db.getTeam(0);
 		Team team2 = db.getTeam(1);
 		Match match = new Match(1,team, team2);
+		Match match2 = new Match(2,team, team2);
+		Match match3 = new Match(3,team, team2);
 		Competition comp = new Competition();
 		comp.add(match);
-		comp.add(match);
-		comp.add(match);
+		comp.add(match2);
+		comp.add(match3);
 		
 		Competition comp2 = new Competition();
 		comp2.add(match);
-		comp2.add(match);
-		comp2.add(match);
+		comp2.add(match2);
+		comp2.add(match3);
 		
 		Competition comp3 = new Competition();
 		comp3.add(match);
-		comp3.add(match);
+		comp3.add(match2);
+		
 		assertTrue(comp.equals(comp2));
 		assertFalse(comp.equals(comp3));
 		assertFalse(comp.equals(team));
@@ -55,8 +91,8 @@ public class CompetitionTest {
 		while(sc.hasNext()){
 			txt +=sc.next();
 		}
-		assertTrue(txt.equals("Competition===========Match( Day: 1, Home: ADO Den Haag, Away: Ajax)Match( Day: 1, Home: ADO Den Haag, Away: Ajax)Match( Day: 1, Home: ADO Den Haag, Away: Ajax)"));
-		assertFalse(txt.equals("Competition===========Match( Match( Day: 1, Home: ADO Den Haag, Away: Ajax)Match( Day: 1, Home: ADO Den Haag, Away: Ajax)"));
+		assertTrue(txt.equals("Competition===========Match( Day: 1, Home: ADO Den Haag, Away: Ajax)"));
+		assertFalse(txt.equals("Competition==========="));
 	}
 	
 	@Test
@@ -75,8 +111,24 @@ public class CompetitionTest {
 		while(sc.hasNext()){
 			txt += sc.next();
 		}
-		assertTrue(txt.equals("<Competition>   <Round>      <Day>1</Day>      <Match>         <Home>ADO Den Haag</Home>         <Away>Ajax</Away>         <Homescore>-1</Homescore>         <Awayscore>-1</Awayscore>      </Match>      <Match>         <Home>ADO Den Haag</Home>         <Away>Ajax</Away>         <Homescore>-1</Homescore>         <Awayscore>-1</Awayscore>      </Match>      <Match>         <Home>ADO Den Haag</Home>         <Away>Ajax</Away>         <Homescore>-1</Homescore>         <Awayscore>-1</Awayscore>      </Match>   </Round>   <Round>      <Day>2</Day>   </Round>   <Round>      <Day>3</Day>   </Round>   <Round>      <Day>4</Day>   </Round>   <Round>      <Day>5</Day>   </Round>   <Round>      <Day>6</Day>   </Round>   <Round>      <Day>7</Day>   </Round>   <Round>      <Day>8</Day>   </Round>   <Round>      <Day>9</Day>   </Round>   <Round>      <Day>10</Day>   </Round>   <Round>      <Day>11</Day>   </Round>   <Round>      <Day>12</Day>   </Round>   <Round>      <Day>13</Day>   </Round>   <Round>      <Day>14</Day>   </Round>   <Round>      <Day>15</Day>   </Round>   <Round>      <Day>16</Day>   </Round>   <Round>      <Day>17</Day>   </Round>   <Round>      <Day>18</Day>   </Round>   <Round>      <Day>19</Day>   </Round>   <Round>      <Day>20</Day>   </Round>   <Round>      <Day>21</Day>   </Round>   <Round>      <Day>22</Day>   </Round>   <Round>      <Day>23</Day>   </Round>   <Round>      <Day>24</Day>   </Round>   <Round>      <Day>25</Day>   </Round>   <Round>      <Day>26</Day>   </Round>   <Round>      <Day>27</Day>   </Round>   <Round>      <Day>28</Day>   </Round>   <Round>      <Day>29</Day>   </Round>   <Round>      <Day>30</Day>   </Round>   <Round>      <Day>31</Day>   </Round>   <Round>      <Day>32</Day>   </Round>   <Round>      <Day>33</Day>   </Round>   <Round>      <Day>34</Day>   </Round>      <Match>         <Home>ADO Den Haag</Home>         <Away>Ajax</Away>         <Homescore>-1</Homescore>         <Awayscore>-1</Awayscore>      </Match>      <Match>         <Home>ADO Den Haag</Home>         <Away>Ajax</Away>         <Homescore>-1</Homescore>         <Awayscore>-1</Awayscore>      </Match>      <Match>         <Home>ADO Den Haag</Home>         <Away>Ajax</Away>         <Homescore>-1</Homescore>         <Awayscore>-1</Awayscore>      </Match></Competition>"));
+		assertTrue(txt.equals("   <Competition>      <Round>         <Day>1</Day>         <Match>            <Home>ADO Den Haag</Home>            <Away>Ajax</Away>            <Homescore>-1</Homescore>            <Awayscore>-1</Awayscore>         </Match>      </Round>      <Round>         <Day>2</Day>      </Round>      <Round>         <Day>3</Day>      </Round>      <Round>         <Day>4</Day>      </Round>      <Round>         <Day>5</Day>      </Round>      <Round>         <Day>6</Day>      </Round>      <Round>         <Day>7</Day>      </Round>      <Round>         <Day>8</Day>      </Round>      <Round>         <Day>9</Day>      </Round>      <Round>         <Day>10</Day>      </Round>      <Round>         <Day>11</Day>      </Round>      <Round>         <Day>12</Day>      </Round>      <Round>         <Day>13</Day>      </Round>      <Round>         <Day>14</Day>      </Round>      <Round>         <Day>15</Day>      </Round>      <Round>         <Day>16</Day>      </Round>      <Round>         <Day>17</Day>      </Round>      <Round>         <Day>18</Day>      </Round>      <Round>         <Day>19</Day>      </Round>      <Round>         <Day>20</Day>      </Round>      <Round>         <Day>21</Day>      </Round>      <Round>         <Day>22</Day>      </Round>      <Round>         <Day>23</Day>      </Round>      <Round>         <Day>24</Day>      </Round>      <Round>         <Day>25</Day>      </Round>      <Round>         <Day>26</Day>      </Round>      <Round>         <Day>27</Day>      </Round>      <Round>         <Day>28</Day>      </Round>      <Round>         <Day>29</Day>      </Round>      <Round>         <Day>30</Day>      </Round>      <Round>         <Day>31</Day>      </Round>      <Round>         <Day>32</Day>      </Round>      <Round>         <Day>33</Day>      </Round>   </Competition>"));
 		assertFalse(txt.equals("<Competition>   <Round>      <Day>1</Day>      <Match>         <Home>ADO Den Haag</Home>         <Away>Ajax</Away>         <Homescore>-1</Homescore>         <Awayscore>-1</Awayscore>      </Match>      <Match>         <Home>ADO Den Haag</Home>         <Away>Ajax</Away>         <Homescore>-1</Homescore>         <Awayscore>-1</Awayscore>      </Match>      <Match>         <Home>ADO Den Haag</Home>         <Away>Ajax</Away>         <Homescore>-1</Homescore>         <Awayscore>-1</Awayscore>      </Match>   </Round>   <Round>      <Day>2</Day>   </Round>   <Round>      <Day>3</Day>   </Round>   <Round>      <Day>4</Day>   </Round>   <Round>      <Day>5</Day>   </Round>   <Round>      <Day>6</Day>   </Round>   <Round>      <Day>7</Day>   </Round>   <Round>      <Day>8</Day>   </Round>   <Round>      <Day>9</Day>   </Round>   <Round>      <Day>10</Day>   </Round>   <Round>      <Day>11</Day>   </Round>   <Round>      <Day>12</Day>   </Round>   <Round>      <Day>13</Day>   </Round>   <Round>      <Day>14</Day>   </Round>   <Round>      <Day>15</Day>   </Round>   <Round>      <Day>16</Day>   </Round>   <Round>      <Day>17</Day>   </Round>   <Round>      <Day>18</Day>   </Round>   <Round>      <Day>19</Day>   </Round>   <Round>      <Day>20</Day>   </Round>   <Round>      <Day>21</Day>   </Round>   <Round>      <Day>22</Day>   </Round>   <Round>      <Day>23</Day>   </Round>   <Round>      <Day>24</Day>   </Round>   <Round>      <Day>25</Day>   </Round>   <Round>      <Day>26</Day>   </Round>   <Round>      <Day>27</Day>   </Round>   <Round>      <Day>28</Day>   </Round>   <Round>      <Day>29</Day>   </Round>   <Round>      <Day>30</Day>   </Round>   <Round>      <Day>31</Day>   </Round>   <Round>      <Day>32</Day>   </Round>   <Round>      <Day>33</Day>   </Round>   <Round>      <Day>34</Day>   </Round>      <Match>         <Home>ADO Den Haag</Home>         <Away>Ajax</Away>         <Homescore>-1</Homescore>         <Awayscore>-1</Awayscore>      </Match>      </Competition>"));
+	}
+	
+	@Test
+	public void testGetIndex(){
+		DBmain db = XmlParser.parseDB();
+		Team team = db.getTeam(0);
+		Team team2 = db.getTeam(1);
+		Match match = new Match(1,team, team2);
+		Match match2 = new Match(2,team, team2);
+		Match match3 = new Match(9,team, team2);
+		Competition comp = new Competition();
+		comp.add(match);
+		comp.add(match2);
+		assertTrue(comp.getIndex(match2) == 1);
+		assertFalse(comp.getIndex(match2) == 2);
+		assertTrue(comp.getIndex(match3) == -1);
 	}
 
 }
