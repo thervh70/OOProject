ab997b7f42e28afd3a6f34d6c58d38f12aa3ab4e
Bram <b.crielaard@student.tudelft.nl>
2015-01-20 16:27:38 +0100

    Budget test
    due to all the if statements which use random it is impossible to get a
    high branch coverage for Budget

--- src/Tests/BudgetTest.java
+++ src/Tests/BudgetTest.java
@@ -5,31 +5,52 @@ import org.junit.Test;
 
 import Controller.Budget;
 import Model.DBmain;
+import Model.Fieldplayer;
+import Model.Goalkeeper;
 import Model.Player;
 import Model.Team;
 import Model.XmlParser;
+import Controller.saveGame;
 
 
 public class BudgetTest {
-
-	/**	
+	
+	saveGame s = new saveGame();
+	DBmain db = XmlParser.parseDB();
+	Team t1 = db.getTeam(1);
+	Team t2 = db.getTeam(2);
+	Player p1 = t1.getPlayer(1);
+	Player p2 = t2.getPlayer(1);
+	
+	
 	@Test
-	public void test() {
-		DBmain test = XmlParser.parseDB();
-		Team frits = test.getTeam(1);
-		Team frits2 = test.getTeam(2);
-		Player testP = frits.getPlayer(0);
-		assertTrue(frits.containsPlayer(testP));
-		assertFalse(frits2.containsPlayer(testP));
-		if(Budget.bid(testP,frits2,frits)==true){
-			assertFalse(frits.containsPlayer(testP));
-			assertTrue(frits2.containsPlayer(testP));
+	public void tosellTest() throws Exception{
+		s.setMyteam(t1);
+		s.setDB(db);
+		assertTrue(t1.containsPlayer(p1));
+		if(Budget.tosell(p1)==null){
+			assertTrue(t1.containsPlayer(p1));
 		}
 		else{
-			assertTrue(frits.containsPlayer(testP));
-			assertFalse(frits2.containsPlayer(testP));
+			assertFalse(t1.containsPlayer(p1));
+		}
+		
+	}
+	
+	@Test
+	public void bidTest() throws Exception{
+		s.setBuyc(0);
+		assertEquals(s.getBuyc(),0);
+		s.setMyteam(t1);
+		assertFalse(t1.containsPlayer(p2));
+		assertTrue(t2.containsPlayer(p2));
+		if(Budget.bid(p2,t2,1)==false){
+			assertFalse(t1.containsPlayer(p2));
+			assertTrue(t2.containsPlayer(p2));
+		}
+		else{
+			assertTrue(t1.containsPlayer(p2));
+			assertFalse(t2.containsPlayer(p2));
 		}
 	}
-	**/
-
 }
