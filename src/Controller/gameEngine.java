463d52c4462480fc5b90dd5a833e200c0f82083d
thervh70 <R.H.vanHeukelum@student.tudelft.nl>
2015-01-22 15:14:16 +0100

    GameEngine: Javadoc

--- src/Controller/gameEngine.java
+++ src/Controller/gameEngine.java
@@ -28,12 +28,17 @@ public class gameEngine {
 	private static ArrayList<Player> injuredPlayerA = new ArrayList<Player>();
 	private static ArrayList<Player> injuredPlayerB = new ArrayList<Player>();
 	
+	/**
+	 * This constructor creates a new instance of the GameEngine.
+	 * That way we are able to get the results from a seperate GameEngine
+	 */
+	
 	public gameEngine(){
 		
 	}
 	
 	/**This method plays a match.
-	 * All stats are stored into static values within this class
+	 * All stats are stored within values of this class
 	 * 
 	 * @param alpha Original Team A
 	 * @param beta Original Team B
@@ -144,6 +149,11 @@ public class gameEngine {
 		saveGame.refreshTeam(beta, teamB);
 	}
 	
+	/**This method generates a toto based on the results from a match
+	 * 
+	 * @return int toto: A won - 1, b won - 2, tie - 0
+	 */
+	
 	@SuppressWarnings("null")
 	public int makeToto() {
 		if(goalsA > goalsB){toto = 1; return toto;}
@@ -155,7 +165,7 @@ public class gameEngine {
 	/**An attack needs 2 values: 1 attacking (Team A) and 1 defending (Team B)
 	 * Int d is calculated first. This value represents the amount of goal attempts.
 	 * The for loop is initiated, with the amount of attempts as max.
-	 * An attacking and a defending score is calculated.
+	 * An attacking and a defending score for 1 attack is calculated.
 	 * This happens randomly, with the teamscore as a basis.
 	 * If the attacking team is lucky, they score the highest.
 	 * 
@@ -217,6 +227,12 @@ public class gameEngine {
 		return minutes;
 	}
 	
+	/**A static method to check whether a set contains x
+	 *  
+	 * @param set an integer array
+	 * @param x the number looked for
+	 * @return a boolean
+	 */
 	public static boolean contains(int[] set, int x){
 		for(int i = 0; i < set.length; i++){
 			if(set[i] == x){
@@ -226,35 +242,93 @@ public class gameEngine {
 		return false;
 	}
 
+	/**@return Get Team A */
 	public Team getTeamA() {return teamA;}
-	public Team getTeamB() {return teamB;}
-	public int getAttemptsA() {return attemptsA;}
-	public int getAttemptsB() {return attemptsB;}
-	public int getGoalsA() {return goalsA;}
-	public int getGoalsB() {return goalsB;}
-	public int getYellowcardsA() {return yellowcardsA;}
-	public int getYellowcardsB() {return yellowcardsB;}
-	public int getRedcardsA() {return redcardsA;}
-	public int getRedcardsB() {return redcardsB;}
-	public int getInjuriesA() {return injuriesA;}
-	public int getInjuriesB() {return injuriesB;}
-	public int getAttempts() {return attempts;}
-	public int getToto() {return toto;}
-	public int[] getGoalminutesA() {return goalminutesA;}
-	public int[] getGoalminutesB() {return goalminutesB;}
-	public int[] getAttemptminutesA() {return attemptminutesA;}
-	public int[] getAttemptminutesB() {return attemptminutesB;}
-	public int[] getYellowcardminutesA() {return yellowcardminutesA;}
-	public int[] getYellowcardminutesB() {return yellowcardminutesB;}
-	public int[] getRedcardminutesA() {return redcardminutesA;}
-	public int[] getRedcardminutesB() {return redcardminutesB;}
-	public int[] getInjuryminutesA() {return injuryminutesA;}
-	public int[] getInjuryminutesB() {return injuryminutesB;}
-	public static ArrayList<Player> getYellowPlayerA() {return yellowPlayerA;}
-	public static ArrayList<Player> getYellowPlayerB() {return yellowPlayerB;}
-	public static ArrayList<Player> getRedPlayerA() {return redPlayerA;}
-	public static ArrayList<Player> getRedPlayerB() {return redPlayerB;}
-	public static ArrayList<Player> getInjuredPlayerA() {return injuredPlayerA;}
-	public static ArrayList<Player> getInjuredPlayerB() {return injuredPlayerB;}
 	
+	/**@return Get Team B */
+	public Team getTeamB() {return teamB;}
+
+	/**@return The total amount of attempts made by team A*/
+	public int getAttemptsA() {return attemptsA;}
+
+	/**@return The total amount of attempts made by team B*/
+	public int getAttemptsB() {return attemptsB;}
+
+	/**@return The total amount of goals made by team A*/
+	public int getGoalsA() {return goalsA;}
+
+	/**@return The total amount of goals made by team B*/
+	public int getGoalsB() {return goalsB;}
+
+	/**@return The total amount of yellow cards of team A*/
+	public int getYellowcardsA() {return yellowcardsA;}
+
+	/**@return The total amount of yellow cards of team B*/
+	public int getYellowcardsB() {return yellowcardsB;}
+
+	/**@return The total amount of red cards of team A*/
+	public int getRedcardsA() {return redcardsA;}
+
+	/**@return The total amount of red cards of team B*/
+	public int getRedcardsB() {return redcardsB;}
+
+	/**@return The total amount of injuries of team A*/
+	public int getInjuriesA() {return injuriesA;}
+	
+	/**@return The total amount of injuries of team B*/
+	public int getInjuriesB() {return injuriesB;}
+
+	/**@return The total amount of attempts*/
+	public int getAttempts() {return attempts;}
+	
+	/**@return The toto of a match*/
+	public int getToto() {return toto;}
+
+	/**@return A array of integers with the minutes Team A scored a goal*/
+	public int[] getGoalminutesA() {return goalminutesA;}
+
+	/**@return A array of integers with the minutes Team B scored a goal*/
+	public int[] getGoalminutesB() {return goalminutesB;}
+
+	/**@return A array of integers with the minutes an attempt was made by Team A*/
+	public int[] getAttemptminutesA() {return attemptminutesA;}
+
+	/**@return A array of integers with the minutes an attempt was made by Team B*/
+	public int[] getAttemptminutesB() {return attemptminutesB;}
+
+	/**@return A array of integers with the minutes players from team A get a yellow card */
+	public int[] getYellowcardminutesA() {return yellowcardminutesA;}
+
+	/**@return A array of integers with the minutes players from team B get a yellow card */
+	public int[] getYellowcardminutesB() {return yellowcardminutesB;}
+
+	/**@return A array of integers with the minutes players from team A get a red card */
+	public int[] getRedcardminutesA() {return redcardminutesA;}
+
+	/**@return A array of integers with the minutes players from team B get a red card */
+	public int[] getRedcardminutesB() {return redcardminutesB;}
+
+	/**@return A array of integers with the minutes players from team A get injured */
+	public int[] getInjuryminutesA() {return injuryminutesA;}
+
+	/**@return A array of integers with the minutes players from team B get injured */
+	public int[] getInjuryminutesB() {return injuryminutesB;}
+
+	/**@return An ArrayList of players from Team A with a Yellow Card */
+	public static ArrayList<Player> getYellowPlayerA() {return yellowPlayerA;}
+
+	/**@return An ArrayList of players from Team B with a Yellow Card */
+	public static ArrayList<Player> getYellowPlayerB() {return yellowPlayerB;}
+
+	/**@return An ArrayList of players from Team A with a Red Card */
+	public static ArrayList<Player> getRedPlayerA() {return redPlayerA;}
+	
+	/**@return An ArrayList of players from Team B with a Red Card */
+	public static ArrayList<Player> getRedPlayerB() {return redPlayerB;}
+	
+	/**@return An ArrayList of injured players from Team A */
+	public static ArrayList<Player> getInjuredPlayerA() {return injuredPlayerA;}
+	
+	/**@return An ArrayList of injured players from Team B */
+	public static ArrayList<Player> getInjuredPlayerB() {return injuredPlayerB;}	
 }
